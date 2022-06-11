19742a1f31b2b8ea9ff7824c099a2fe1e522e0f7
bishal0602 <bishalpanta60@gmail.com>
2022-06-11 21:17:57 +0545

    aesthetic changes

--- client/src/components/CreateTodo.jsx
+++ client/src/components/CreateTodo.jsx
@@ -3,14 +3,14 @@ import { FiArrowRightCircle } from "react-icons/fi";
 
 const CreateTodo = () => {
   return (
-    <div className="h-[3rem] w-[90%] self-center mx-auto flex items-center justify-between text-xl text-center mb-8 border-2 border-solid border-violet-400 rounded-xl shadow-lg shadow-purple-300">
+    <div className="h-[3rem] w-[90%] self-center mx-auto flex items-center justify-between text-xl text-center mb-8 border-2 border-solid border-violet-400 rounded-xl shadow-sm shadow-purple-300">
       <input
         type="text"
         placeholder="Enter To-do"
         className="h-4/5 w-[87%] p-2 border-none bg-transparent"
       ></input>
       <FiArrowRightCircle
-        className="h-4/5 w-[10%] text-violet-500 hover:text-green-500"
+        className="h-4/5 w-[10%] text-violet-500 hover:text-pink-600"
         onClick={() => alert("Sorry the feature is not ready yet:(")}
       />
     </div>
--- client/src/components/ToDo.jsx
+++ client/src/components/ToDo.jsx
@@ -4,14 +4,14 @@ import { MdOutlineDoneOutline } from "react-icons/md";
 const ToDo = ({ title, key, id }) => {
   return (
     <div
-      className="group self-top justify-self-center flex items-center justify-start text-slate-700 min-h-[2.5rem] w-[90%] border-2 border-solid border-purple-500 rounded-xl shadow-md  hover:text-black hover:shadow-purple-500"
+      className="group self-top justify-self-center flex items-center justify-start text-slate-700 min-h-[2.5rem] w-[90%] border-2 border-solid border-purple-500 rounded-xl shadow-sm shadow-pink-200  hover:text-black hover:shadow-purple-500 hover:border-pink-600 hover:shadow-md"
       key={key}
       id={id}
       onClick={() => alert("Sorry the feature is not ready yet:(")}
     >
       <MdOutlineDoneOutline
         size={20}
-        className="ml-2 group-hover:text-lime-700 group-hover:scale-110 "
+        className="ml-2 group-hover:text-pink-600 group-hover:scale-110 "
       />
       <h1 className="ml-2 text-left  ">{title}</h1>
     </div>
